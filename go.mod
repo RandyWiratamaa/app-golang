af5dbbd131763994a23f33c3a033366495ec101e
tebetebe19 <randywiratama1@gmail.com>
2022-09-01 10:58:17 +0700

    Import Golang Module

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/RandyWiratamaa/app-golang
 
 go 1.18
+
+require github.com/RandyWiratamaa/golang-module v1.0.1
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	golang_module "github.com/RandyWiratamaa/golang-module" // import module
+)
+
+func main() {
+	fmt.Println(golang_module.SayHello())
+}
